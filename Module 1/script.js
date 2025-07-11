7e2bb286e77923f0feac98e6f02d72d8176164d8
ecook75 <eppeycook2@gmail.com>
2025-07-11 17:18:30 +1200

    merge commit

---
+++ Module 1/script.js
@@ -0,0 +1,21 @@
+// this is the structure of a function
+// function ThisIsTheName(Signature) {
+// Body
+// }
+function sayHello() {
+    console.log('Hello')
+    }
+
+    sayHello()
+
+    function sum(a, b) {
+        console.log(a + b)
+        }
+
+        sum(3, 4)
+
+        function returnSum(a, b) {
+            return a + b
+            }
+
+            console.log(returnSum(2, 8))
